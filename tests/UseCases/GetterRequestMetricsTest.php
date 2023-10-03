d294f70f8b86d7f64144bb15cff5a0fb08c60635
Vasiliy Nikiforov <kaktusv6@gmail.com>
2023-10-03 22:19:27 +1000

    Create tests for use cases

---
+++ tests/UseCases/GetterRequestMetricsTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Tests\UseCases;
+
+use App\Entities\AccessLog;
+use App\Metrics\ICollector;
+use App\Metrics\ICounter;
+use App\Metrics\IHistogram;
+use App\Repositories\IAccessLogRepository;
+use App\UseCases\GetterRequestMetrics;
+use Carbon\Carbon;
+use Faker\Factory;
+use Faker\Generator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
+final class GetterRequestMetricsTest extends TestCase
+{
+    private Generator $faker;
+
+    private IAccessLogRepository $repository;
+    private ICounter $counter;
+    private IHistogram $histogram;
+    private ICollector $collector;
+
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->faker = Factory::create();
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->repository = $this->createMock(IAccessLogRepository::class);
+
+        $this->counter = $this->createMock(ICounter::class);
+        $this->histogram = $this->createMock(IHistogram::class);
+        $this->collector = $this->createMock(ICollector::class);
+    }
+
+    public function dataProviderLogs(): array
+    {
+        return [
+            'first test' => [$this->generateLogs(6)],
+            'second test' => [$this->generateLogs(10)],
+            'one log' => [$this->generateLogs(1)],
+            'empty logs' => [[]],
+        ];
+    }
+
+    /** @dataProvider dataProviderLogs */
+    public function testSimple(array $logs): void
+    {
+        $logsChunks = array_chunk($logs, 5);
+
+        $this->repository
+            ->expects($this->once())
+            ->method('getChunksGenerator')
+            ->will($this->returnCallback(function () use ($logsChunks) {
+                foreach ($logsChunks as $chunk) {
+                    yield $chunk;
+                }
+            }))
+        ;
+
+        $countLogs = count($logs);
+
+        $this->counter
+            ->expects($this->exactly($countLogs))
+            ->method('inc')
+        ;
+
+        $this->histogram
+            ->expects($this->exactly($countLogs))
+            ->method('set')
+        ;
+
+        $this->collector
+            ->expects($this->exactly($countLogs))
+            ->method('getCounter')
+            ->willReturn($this->counter)
+        ;
+
+        $this->collector
+            ->expects($this->exactly($countLogs))
+            ->method('getHistogram')
+            ->willreturn($this->histogram)
+        ;
+
+        $getter = new GetterRequestMetrics(
+            $this->repository,
+            $this->collector,
+        );
+
+        $collector = $getter->get();
+        $this->assertEquals($this->collector, $collector);
+    }
+
+    private function generateLogs(int $count): array
+    {
+        $startedAt = Carbon::now()->days(-1);
+
+        $result = [];
+
+        while (count($result) < $count) {
+            $result[] = (new AccessLog())
+                ->setStatus($this->faker->randomElement([200, 500]))
+                ->setTimeMilliseconds($this->faker->randomFloat(1, 50))
+                ->setCreatedAt($startedAt)
+            ;
+            $startedAt->addSeconds($this->faker->randomElement([0, 1]));
+        }
+
+        return $result;
+    }
+}
